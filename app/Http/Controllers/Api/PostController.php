694e88ffb1ed0621122b61918e4e36b88c75e186
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-07-14 14:50:26 +0200

    aggiunto category all'interno di api/postcontroller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     //
     public function index() {
-        $posts = Post::paginate(8);
+        $posts = Post::with(['category'])->paginate(6);
         return response()->json([
             'success' => true,
             'results' => $posts
