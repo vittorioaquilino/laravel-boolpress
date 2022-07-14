6e5d8aa0f143ee91b08870f198369d4035557f1c
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-07-14 15:37:13 +0200

    creata rotta show e implementata funzione show in postcontroller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -16,4 +16,18 @@ class PostController extends Controller
             'results' => $posts
         ]);
     }
+
+    public function show($slug) {
+        $post = Post::where('slug', '=', $slug)->with(['category'])->first();
+        if($post) {
+            return response()->json([
+                'success' => true,
+                'results' => $post
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'error' => 'Nessun post trovato'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'Api\PostController@index')->name('api.posts');
+Route::get('/posts', 'Api\PostController@index')->name('api.posts.index');
+Route::get('/posts/{slug}', 'Api\PostController@show')->name('api.posts.show');
